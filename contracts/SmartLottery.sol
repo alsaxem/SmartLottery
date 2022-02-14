df95b3826ed37d7fd67ff00cc77cb3a376fd6e64
Alsaxem <alsaxem2015@gmail.com>
2022-02-14 23:09:07 +0200

    feat: add empty project template

---
+++ contracts/SmartLottery.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 < 0.9.0;
+contract SmartLottery {
+
+    // Сontract owner's address.
+    address owner;
+
+    // Lottery winner address.
+    address public winner;
+
+    // Ether limit on the balance of the contract.
+    // The value is determined when the contract is initialized.
+    uint256 maxBalance;
+
+    // Lottery end time.
+    // The value is determined when the contract is initialized.
+    uint256 endTime;
+
+    // List of lottery participants.
+    address[] public players;
+
+    // Number of tickets assigned to a specific address.
+    mapping (address => uint) public ticketsBalances;
+ 
+    // Set to true at the end, disallows any change.
+    // By default initialized to `false`.
+    bool ended;
+
+    // Event that will be emitted on end.
+    event LotteryEnded(address, uint256);
+
+    /// The lottery has already ended.
+    error LotteryAlreadyEnded();
+
+    /// The lottery has not ended yet.
+    error LotteryNotYetEnded();
+
+    /// The lottery has not ended yet.
+    error LotteryBalanceOverflow(string, uint256);
+
+    /**
+    * @notice Create a new lottery with given parameters.
+    * @dev sets owner to the current sender and default value for ended
+    * @dev set maxBalance to the maximum number of tickets
+    * @dev set endTime to the sum of the block.timestamp and duration
+    * @param _maxEth total number of tickets
+    * @param _duration time period in seconds
+    */
+    constructor(uint256 _maxEth, uint _duration) {}
+
+    /**
+    * @notice Buying tickets with Ether.
+    * @dev tokens are credited to the user's account
+    */
+    function buyTickets() external payable {}
+
+    /**
+    * @dev generate random ticket with keccak256
+    */
+    function randomTicket() internal view returns(uint256) {}
+
+    /**
+    * @dev pick winner with random ticket
+    */
+    function winnerPick() internal {}
+
+    /**
+    * @notice The end of the lottery and the transfer of ethereum.
+    * @dev сommission and reward calculation
+    * @dev determination of the winner and transfer of ethereum
+    * @dev changing the value of a variable ended to true
+    * @dev event emission LotteryEnded
+    */
+    function lotteryEnd() external {}
+}
