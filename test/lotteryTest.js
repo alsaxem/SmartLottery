607265b71bcb24b164802a5465cee26546be3542
Alsaxem <alsaxem2015@gmail.com>
2022-02-15 21:54:27 +0200

    test: add lottery test template

---
+++ test/lotteryTest.js
@@ -0,0 +1,39 @@
+const { expect } = require("chai");
+
+
+describe("Lottery contract", function () {
+    
+    //Contract factory
+    let Token;
+
+    //Current contract instance
+    let smartLottery;
+
+    //Test account addresses
+    let owner;
+    let addr1;
+    let addr2;
+    let addrs;
+
+    //Ethereum limit for the lottery
+    let ethLimit;
+
+    //Contract initialization time
+    let deploymentTime;
+
+    //Time limit for the lottery
+    let duration;
+
+    //Perform initial steps for all tests
+    beforeEach(async function () {});
+
+    //Contract initialization testing
+    describe("Deployment", function () {});
+
+    //Testing the function of buying tokens to participate in the lottery
+    describe("Buying Tickets", function () {});
+
+    //Testing the Lottery End Function
+    describe("Ending Lottery", function () {});
+
+});
