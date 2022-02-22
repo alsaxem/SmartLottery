e4f246fd1fcd31aab2404ca7ffb93aa88e75d511
Alsaxem <alsaxem2015@gmail.com>
2022-02-22 22:31:22 +0200

    feat(SLToken): add empty project template

---
+++ contracts/SLToken.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >= 0.8.2 < 0.9.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SLToken is ERC20 {
+    // Owner account address.
+    address public owner;
+
+    /**
+    * @notice Create Tokens.
+    * @param _initialSupply initial token supply to be minted
+    * @dev sets owner to the current sender (deployer)
+    * @dev mints initial token supply and assigns it to the owner
+    */
+    constructor(uint _initialSupply) ERC20("SLToken", "SLT") {}
+
+    /** 
+    * @notice Transfer tokens to another account.
+    * @param recipient address of the receiver
+    * @param amount number of tokens to be transferred
+    * @return true if transaction was successful
+    */
+    function transfer(address recipient, uint256 amount) public override returns (bool) {}
+
+    /** 
+    * @notice Exchange tokens to lottery tickets.
+    * @param lotteryAddr address of the lottery
+    * @param amount number of tickets to be exchange
+    * @return true if transaction was successful
+    */
+    function exchangeForTickets(address lotteryAddr, uint256 amount) external returns (bool) {}
+}
