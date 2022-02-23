c79f1ef720e6a9ac37174bfc12723fca242520da
Alsaxem <alsaxem2015@gmail.com>
2022-02-23 04:10:04 +0200

    feat(SLToken): add tickets exchange

--- contracts/SLToken.sol
+++ contracts/SLToken.sol
@@ -3,6 +3,7 @@
 pragma solidity >= 0.8.2 < 0.9.0;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./SmartLottery.sol";
 
 contract SLToken is ERC20 {
     // Owner account address.
@@ -21,22 +22,30 @@ contract SLToken is ERC20 {
 
     /** 
     * @notice Transfer tokens to another account.
-    * @param recipient address of the receiver
-    * @param amount number of tokens to be transferred
+    * @param _recipient address of the receiver
+    * @param _amount number of tokens to be transferred
     * @return true if transaction was successful
     */
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
-        require(amount > 0, "Token amount must be greater than 0");
-        require(balanceOf(msg.sender) >= amount, "Not enough tokens");
-        _transfer(_msgSender(), recipient, amount);
+    function transfer(address _recipient, uint256 _amount) public override returns (bool) {
+        require(_amount > 0, "Token amount must be greater than 0");
+        require(balanceOf(msg.sender) >= _amount, "Not enough tokens");
+        _transfer(_msgSender(), _recipient, _amount);
         return true;
     }
 
     /** 
     * @notice Exchange tokens to lottery tickets.
-    * @param lotteryAddr address of the lottery
-    * @param amount number of tickets to be exchange
+    * @param _lotteryAddr address of the lottery
+    * @param _amount number of tickets to be exchange
     * @return true if transaction was successful
     */
-    function exchangeForTickets(address lotteryAddr, uint256 amount) external returns (bool) {}
+    function exchangeForTickets(address _lotteryAddr, uint256 _amount) external returns (bool) {
+        require(_amount > 0, "Tikets amount must be greater than 0");
+        SmartLottery sLottery = SmartLottery(_lotteryAddr);
+        uint ticketsPrice = sLottery.ticketPrice() * _amount;
+        require(balanceOf(msg.sender) >= ticketsPrice, "Not enough tokens");
+        approve(_lotteryAddr, _amount);
+        sLottery.creditTickets(msg.sender , _amount);
+        return true;
+    }
 }
