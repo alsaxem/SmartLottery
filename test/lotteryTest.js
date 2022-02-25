092ffa2390742375f3436c5ead44f7722ab1da8a
Alsaxem <alsaxem2015@gmail.com>
2022-02-26 01:57:17 +0200

    test: update lottery deployment tests

--- test/lotteryTest.js
+++ test/lotteryTest.js
@@ -62,70 +62,33 @@ describe("Lottery contract", function () {
       });
     });
 
-    //Testing the function of buying tokens to participate in the lottery
-    describe("Buying tickets", function () {
+    //Lottery contract initialization testing
+    describe("Lottery deployment", function () {
 
-        it("Should fail if the transaction does not contain ether", async function () {
-          await expect( 
-            smartLottery.connect(addr1).buyTickets({value: 0})
-          ).to.be.revertedWith("Ether amount must be greater than 0");
-        });
-      
-        it("Should increase user token balance after the purchase", async function () {
-          await smartLottery.connect(addr1).buyTickets({value: ethers.utils.parseEther("100")});
-          const tockensBalance = await smartLottery.ticketsBalances(addr1.address);
-          expect(tockensBalance).to.equal(ethers.utils.parseEther("100"));
-        });
-    
-        it("Should fail if lottery time is up", async function () {
-          await network.provider.send("evm_increaseTime", [duration+1]);
-          await expect( 
-            smartLottery.connect(addr1).buyTickets({value: ethers.utils.parseEther("100")})
-          ).to.be.revertedWith("LotteryAlreadyEnded");
-        });
-    
-        it("Should fail if lottery has been ended", async function () {
-          await smartLottery.connect(addr1).buyTickets({value: ethers.utils.parseEther("1000")});
-          await smartLottery.connect(addr1).lotteryEnd();
-          await expect(
-            smartLottery.connect(addr1).buyTickets({value: ethers.utils.parseEther("100")})
-          ).to.be.revertedWith("LotteryAlreadyEnded");
-        });
-    
-        it("Should fail in case of ticket balance overflow", async function () {
-          await smartLottery.connect(addr1).buyTickets({value: ethers.utils.parseEther("1000")});
-          await expect(
-            smartLottery.connect(addr1).buyTickets({value: ethers.utils.parseEther("100")})
-          ).to.be.revertedWith("LotteryBalanceOverflow");
-        });
-    
-    });
-
-    //Testing the Lottery End Function
-    describe("Ending Lottery", function () {
-
-        it("Should transfer 90% of the contract balance to the winner", async function () {
-          await smartLottery.connect(addr1).buyTickets({value: ethers.utils.parseEther("1000")});
-          await expect(smartLottery.lotteryEnd())
-          .to.emit(smartLottery, "LotteryEnded")
-          .withArgs(addr1.address, ethers.utils.parseEther("900"));
-        });
-    
-        it("Should be reverted with an error 'LotteryNotYetEnded'", async function () {
-          await smartLottery.connect(addr1).buyTickets({value: ethers.utils.parseEther("100")});
-          await expect( 
-            smartLottery.lotteryEnd()
-          ).to.be.revertedWith("LotteryNotYetEnded");
-        });
-    
-        it("Should fail if lottery has been already ended", async function () {
-          await smartLottery.connect(addr1).buyTickets({value: ethers.utils.parseEther("1000")});
-          await smartLottery.lotteryEnd();
-          await expect(
-            smartLottery.lotteryEnd()
-          ).to.be.revertedWith("LotteryAlreadyEnded");
-        });
-        
+      it("Should set the right end time", async function () {
+        const endTime = await smartLottery.endTime();
+        expect(endTime).to.equal(duration+deploymentTime);
       });
+      
+      it("Should set the right ticket limit", async function () {
+        const maxBalance = await smartLottery.remainingTickets();
+        expect(maxBalance).to.equal(ticketLimit);
+      });
+
+      it("Should set the right owner", async function () {
+        const ownerContract = await smartLottery.owner();
+        expect(ownerContract).to.equal(owner.address);
+      });
+
+      it("Should set the right token address", async function () {
+        const tokenAddr = await smartLottery.tokenContractAddr();
+        expect(tokenAddr).to.equal(SLToken.address);
+      });
+
+      it("Should set the right ticket price", async function () {
+        const ticketLotteryPrice = await smartLottery.ticketPrice();
+        expect(ticketLotteryPrice).to.equal(ticketPrice);
+      });
+    });
 
 });
